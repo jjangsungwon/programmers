4e1e421635c36b9b83f3720693a9e681d585be07
jjangsungwon <dnjs2113@gmail.com>
2020-04-23 20:51:24 +0900

    set 방법 추가

--- sql/입양 시각 구하기(1).sql
+++ sql/입양 시각 구하기(1).sql
@@ -2,3 +2,13 @@ SELECT hour(datetime), count(*)
 FROM animal_outs
 WHERE hour(datetime) >= 9 and hour(datetime) <= 19
 GROUP BY hour(datetime)
+
+/*
+SET @hour := 8;
+
+SELECT
+    (@hour := @hour + 1),
+    (SELECT count(*) FROM animal_outs WHERE hour(datetime) = @hour)
+FROM animal_outs
+WHERE @hour < 19
+*/
